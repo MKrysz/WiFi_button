4450d3f0521ebeb1cdedb7a6b84a35217ae1c528
MKrysz <mkrysz@student.agh.edu.pl>
2021-10-02 20:04:30 +0200

    Working prototype

--- WiFi_button_code/src/main.cpp
+++ WiFi_button_code/src/main.cpp
@@ -1,12 +1,14 @@
 
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
 #include <stdio.h>
 
 const int bled = 12;
 const int rled = 4;
 const float vol_div = 0.175;
-const char serversIP[] = "http://192.168.1.102:80";
+const char serverName[] = "http://192.168.1.102:80";
 const char WIFI_ssid[] = "Y'Ghatan";
 const char WIFI_pass[] = "06094829";
 
@@ -17,13 +19,13 @@ int connectToWiFi();
 
 void setup() {
   Serial.begin(9600);
+  Serial.println('\n');
   pinMode(rled, OUTPUT);
   pinMode(bled, OUTPUT);
   checkVoltage();
   digitalWrite(bled, HIGH);
   connectToWiFi();
   sendPOST_toWebCounter("Nadchwyt", 6);
-  delay(100);
   digitalWrite(bled, LOW);
   Serial.println("Going into deep sleep mode");
   ESP.deepSleep(0); 
@@ -39,9 +41,16 @@ void loop() {
 int sendPOST_toWebCounter(const char exercise[32], int number) 
 {
   char requestData[128];
+  WiFiClient client;
+  HTTPClient http;
   sprintf(requestData, "%s_number=%u&%s=%%2B", exercise, number, exercise);
   Serial.println("requestForm:");
   Serial.println(requestData);
+  http.begin(client, serverName);
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+  Serial.println("HTTP Response:");
+  Serial.println(http.POST(requestData));
+  http.end();
   return 0;
 }
 
@@ -52,8 +61,10 @@ int sendPOST_toWebCounter(const char exercise[32], int number)
 int checkVoltage() 
 {
   const float lowBatThreshold = 3.5;
-
-  float readVoltage = ((analogRead(A0)/1023)*(1/vol_div));
+  int rawRead = analogRead(A0);
+  float readVoltage = (((float)rawRead/1023.00)*(1/vol_div));
+  Serial.println(rawRead);
+  Serial.println(readVoltage);
   if(readVoltage < lowBatThreshold){
     warnLowVoltage();
     return 1;
@@ -80,7 +91,7 @@ int connectToWiFi()
   Serial.print(WIFI_ssid);
   Serial.println("...");
   while (WiFi.status() != WL_CONNECTED) { // Wait for the Wi-Fi to connect
-    delay(1000);
+    delay(250);
     Serial.print('.');
   }
   Serial.println('\n');
